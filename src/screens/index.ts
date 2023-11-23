2fa4a159ce27eaf6ced7f3fae4dc66f9a842ba0b
Prashant <prashant@pearlthoughts.com>
2023-11-23 19:22:40 +0530

    first commit

--- src/screens/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-export { default as Login } from './login';
-export { default as InitialScreen } from './initial-screen';
-export { default as OtpScreen } from './otp-screen';
-export { default as HomeScreen } from './home-screen';
-export { default as DoctorClinic } from './doctorclinic-screen';
-export { default as AppointmentScreen } from './appointment-screen';
